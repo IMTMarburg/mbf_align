10cc445da2ab20de6ba16de6c4a4969df81b6205
Unknown <finkernagel@imt.uni-marburg.de>
2020-07-09 10:42:04 +0200

    build_fastq_strategy now correctly takes a list and does what it says in the docstring

--- src/mbf_align/strategies.py
+++ src/mbf_align/strategies.py
@@ -17,7 +17,7 @@ def build_fastq_strategy(input_strategy):
             str/Path - file: treat as single fastq(.gz) file
             str/Path - folder: treat as folder
             pypipegraph job: extract filenames
-            list - recurvively apply build_fastq_strategy
+            list - recurvively apply build_fastq_strategy and FASTQsJoin them
         """
 
     if isinstance(input_strategy, _FASTQsBase):
@@ -31,12 +31,7 @@ def build_fastq_strategy(input_strategy):
     elif isinstance(input_strategy, ppg.FileGeneratingJob):
         input_strategy = FASTQsFromJob(input_strategy)
     elif isinstance(input_strategy, Iterable):
-        for x in input_strategy:
-            if not (isinstance(x, str) or isinstance(x, Path)):  # pragma: no cover
-                raise ValueError(
-                    "if your input strategy is an iterable, entries must be str/Path"
-                )
-        input_strategy = FASTQsFromFiles([Path(p) for p in input_strategy])
+        input_strategy = FASTQsJoin([build_fastq_strategy(p) for p in input_strategy])
 
     else:
         raise ValueError(f"Could not parse input_strategy: {repr(input_strategy)}")
@@ -299,7 +294,7 @@ class _FASTQsFromSRA(_FASTQsBase):
             stdout, stderr = p.communicate()
             (self.target_dir / "stdout").write_bytes(stdout)
             (self.target_dir / "stderr").write_bytes(stderr)
-            if p.returncode != 0 or b'invalid accession' in stderr:
+            if p.returncode != 0 or b"invalid accession" in stderr:
                 raise ValueError("fasterq-dump", p.returncode)
             (self.target_dir / "sentinel").write_text("done")
 
